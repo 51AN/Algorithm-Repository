b0f9908a98c2389d622ea13781d9124c4a2573a1
51AN <sianashsad@iut-dhaka.edu>
2023-04-11 13:19:42 +0600

    lab test

--- Lab 08 test 2/200042151_L08_T01_A.cpp
+++ Lab 08 test 2/200042151_L08_T01_A.cpp
@@ -13,28 +13,47 @@ using namespace std;
 #define YES                                              cout<<"YES"<<endl
 #define NO                                               cout<<"NO"<<endl
 #define INF 0x3F3F3F3F
-const int MAXN = 100;
-int n, colors[MAXN];
-int dp[MAXN][MAXN];
+int n;
+int mixtures[100];
+int color[100][100];
+int dp[100][100];
 
-int main() {
+int smokeAmount(int i, int j)
+{
+    if(i==j) return 0;
+    if(dp[i][j] != -1) return dp[i][j];
 
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> colors[i];
-
-
-    memset(dp, 0, sizeof(dp));
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n-len; i++) {
-                dp[i][j] = INT_MAX;
-                for (int k = i; k < j; k++) {
-                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + colors[i] * colors[k+1] * colors[j]);
-                }
-            }
-        }
-
-    cout << dp[0][n-1] << endl;
+    dp[i][j] = INT_MAX;
+    for(int k=i; k<j; k++)
+    {
+        dp[i][j] = min(dp[i][j], smokeAmount(i, k) + smokeAmount(k+1,j) + color[i][k]*color[k+1][j]);
     }
+
+    return dp[i][j];
+}
+
+int main()
+{
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>mixtures[i];
+    }
+
+    memset(dp, -1, sizeof(dp));
+    for(int i=0; i<n; i++)
+        color[i][i] = mixtures[i];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            color[i][j] = (mixtures[i] + mixtures[j])%100;
+            color[j][i] = color[i][j];
+        }
+    }
+
+    cout<<smokeAmount(0, n-1)<<endl;
     return 0;
 }
